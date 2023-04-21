f32526f821de7b5f4a3f195d1fb33b8722c44c27
unknown <Sebashj>
2023-04-21 11:04:25 -0500

    hola1

---
+++ 04 Cambiar/app.js
@@ -0,0 +1,10 @@
+const uno=()=>{
+    document.querySelector("#img").src="img1.jfif"
+}
+
+const dos=()=>{
+    document.querySelector("#img").src="img2.jfif"
+}
+const tres=()=>{
+    document.querySelector("#img").src="img3.jfif"
+}
